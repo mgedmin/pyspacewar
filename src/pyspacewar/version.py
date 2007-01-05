3f4c6099751c6a146cf0e58ce9a9199010ba5806
Marius Gedminas <marius@gedmin.as>
2007-01-05 02:21:13 +0000

    Force a change to update the version number.

--- src/pyspacewar/version.py
+++ src/pyspacewar/version.py
@@ -13,3 +13,4 @@ if version.endswith('svn'):
     # revision number for version.py, not the revision number of the whole
     # repository.  Using os.popen('svnversion').read() would be better, but
     # only if the end-user has subversion installed.
+
