a283156c64160ce8fe340af154bf0eda0900da3f
Marius Gedminas <marius@gedmin.as>
2019-05-01 16:41:42 +0300

    Tests for GameUI.controls_menu()

--- src/pyspacewar/tests/test_ui.py
+++ src/pyspacewar/tests/test_ui.py
@@ -2562,6 +2562,19 @@ def doctest_GameUI_screen_resolution_menu():
     """
 
 
+def doctest_GameUI_controls_menu():
+    """Test for GameUI.controls_menu
+
+        >>> from pyspacewar.ui import GameUI
+        >>> ui = GameUI()
+        >>> ui.init()
+        >>> ui.controls_menu()
+        >>> ui.ui_mode
+        <ControlsMenuMode>
+
+    """
+
+
 @pytest.yield_fixture(autouse=True)
 def _pytest_setup(doctest_namespace):
     fake_test = mock.Mock()
